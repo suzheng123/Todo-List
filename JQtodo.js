c280511aa423f99b0174fe86ec68ee91f707aba1
suzheng123 <suzheng@ucsc.edu>
2018-07-18 23:18:03 -0700

    Todo List

---
+++ JQtodo.js
@@ -0,0 +1,25 @@
+//check off/back li by click
+$("ul").on("click","li", function(){
+	$(this).toggleClass("completed");
+})
+
+//click X to delete todo
+$("ul").on("click","span",(function(e){
+	//parent() indicates the li where the clicked span locates in
+	$(this).parent().fadeOut(500,function(){ //'this' refers to span
+		$(this).remove(); //'this' refers to the li
+	}); 
+	e.stopPropagation();//add event e to stop the 
+}))						//click effect to parent
+
+$("input[type='text']").keypress(function(e2){
+	if(e2.which===13) { //keycode for 'enter' is 13
+		var todoText= $(this).val()//grab todo text
+		$(this).val("")
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> "+todoText+"</li>")
+	}
+})
+
+$("#pencil").click(function(){
+	$("input[type='text']").fadeToggle();
+})
